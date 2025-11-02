e36b5c7127b4df634d957a5a1e279ef352f19f74
u2usvc <kwstisulvia@hotmail.com>
2025-11-02 14:23:56 +0300

    add L-121-2_122-2 and modify naming convention for nodes in domain_map; require explicit CIDR in domain_map

--- terraform/networks.tf
+++ terraform/networks.tf
@@ -1,10 +1,3 @@
-# locals {
-#   # leave only unique "net" (i.e. L-111, L-112, L-113)
-#   network_ids = distinct([
-#     # extract "net" from each
-#     for each in values(var.domain_map) : each.net
-#   ])
-# }
 locals {
   # leave only unique "net" (i.e. L-111, L-112, L-113)
   # flatten merges all recursive list into a flat list 
@@ -18,25 +11,15 @@ locals {
 resource "libvirt_network" "links" {
   for_each = toset(local.network_ids)
 
-  # key == link-111
-  name = each.key
-  mode = "none"
-  # bridge == mtcivnbr111
+  name   = each.key
+  mode   = "none"
   bridge = "mtbr${replace(each.key, "L-", "")}"
-  # bridge = replace(each.key, "L-", "mtbr")
 
-  # for initial ip provisioning to hosts for ansible connectivity
-  # this block ensures that networks are created with only unique matching CIDRs
-  # compact() removes null elements from a list
-  # slice(list, start_index, end_index) leaves only specified elements in a list 
-  addresses = slice(compact(
-    [
-      # Access the network configuration based on the net value in the domain_map
-      for key, value in var.domain_map :
-      # if value.net (var.domain_map.value.net) == each.key (network_ids), then return value.cidr (var.domain_map.value.cidr)
-      value.net[0] == each.key ? value.cidr : null
-    ]
-  ), 0, 1)
+  # pick the CIDR that corresponds to this network (as a single string)
+  addresses = slice(compact([
+    for key, value in var.domain_map :
+    contains(value.net, each.key) ? value.cidr[index(value.net, each.key)] : null
+  ]), 0, 1)
 
   dhcp {
     enabled = true
